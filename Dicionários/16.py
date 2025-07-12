8b840fd8eaff93a589545dcc0c84ee6a198fa1c6
Godbless20 <borchevhy@gmail.com>
2025-07-12 19:07:26 -0300

    exo16

---
+++ Dicionários/16.py
@@ -0,0 +1,4 @@
+#Faça um programa que seja uma agenda de compromissos e:
+#◦ Crie e leia um vetor de 5 estruturas de dados com: compromisso e data. A data deve ser outra estrutura de dados contendo dia, mês e ano.
+#◦ Leia dois inteiros M e A e mostre todos os compromissos do mês M do ano A, ordenados do menor para o maior dia.
+#Repita o procedimento até ler M = 0.
